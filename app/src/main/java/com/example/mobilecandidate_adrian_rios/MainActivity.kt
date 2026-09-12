905de142baf0e6afbaddb6959763ff444870d8d9
noc3tis <adrain150604@gmail.com>
2026-09-12 11:10:27 -0600

    Proyecto creado.

---
+++ app/src/main/java/com/example/mobilecandidate_adrian_rios/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.mobilecandidate_adrian_rios
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mobilecandidate_adrian_rios.ui.theme.MobileCandidate_Adrian_RiosTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MobileCandidate_Adrian_RiosTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MobileCandidate_Adrian_RiosTheme {
+        Greeting("Android")
+    }
+}
